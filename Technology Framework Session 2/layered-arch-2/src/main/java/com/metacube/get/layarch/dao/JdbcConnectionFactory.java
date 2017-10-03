8635a69c2f22f4536a0975ae8aedc46d20f6920f
udit saxena <meta-udit-saxena>
2017-10-03 16:08:00 +0530

    Technology Framework Session 2 Assignment

---
+++ Technology Framework Session 2/layered-arch-2/src/main/java/com/metacube/get/layarch/dao/JdbcConnectionFactory.java
@@ -0,0 +1,31 @@
+package com.metacube.get.layarch.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ * A factory for creating JdbcConnection objects.
+ */
+public class JdbcConnectionFactory {
+
+	/**
+	 * Gets the connection.
+	 *
+	 * @return the connection
+	 * @throws Exception
+	 *             the exception
+	 */
+	Connection getConnection() throws Exception {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection con = DriverManager
+					.getConnection("jdbc:mysql://localhost:3306/shopping cart",
+							"root", "root");
+
+			return con;
+		} catch (Exception e) {
+			System.out.println(e);
+			throw e;
+		}
+	}
+}
