f9d5aa85227e59b743a14b5d756b53586c26c75c
udit saxena <meta-udit-saxena>
2017-10-04 19:08:10 +0530

    Update with testcases

---
+++ Technology Framework Session 3/layered-arch-3/src/main/java/com/metacube/shoppingcart/dao/product/ProductDao.java
@@ -0,0 +1,11 @@
+package com.metacube.shoppingcart.dao.product;
+
+import com.metacube.shoppingcart.dao.AbstractDao;
+import com.metacube.shoppingcart.model.Product;
+
+/**
+ * The Interface ProductDao.
+ */
+public interface ProductDao extends AbstractDao<Product, Integer> {
+
+}
---
+++ Technology Framework Session 3/layered-arch-3/src/main/java/com/metacube/shoppingcart/service/DefaultProductService.java
@@ -0,0 +1,117 @@
+package com.metacube.shoppingcart.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import com.metacube.shoppingcart.dao.product.ProductDao;
+import com.metacube.shoppingcart.enums.Status;
+import com.metacube.shoppingcart.model.Product;
+
+/**
+ * The Class DefaultProductService.
+ */
+@Component("productService")
+public class DefaultProductService implements ProductService {
+
+	/** The product dao. */
+	ProductDao productDao;
+
+	/**
+	 * Instantiates a new default product service.
+	 *
+	 * @param productDao
+	 *            the product dao
+	 */
+	@Autowired
+	public DefaultProductService(ProductDao productDao) {
+		this.productDao = productDao;
+	}
+
+	/**
+	 * Gets the product dao.
+	 *
+	 * @return the product dao
+	 */
+	public ProductDao getProductDao() {
+		return productDao;
+	}
+
+	/**
+	 * Sets the product dao.
+	 *
+	 * @param productDao
+	 *            the new product dao
+	 */
+	public void setProductDao(ProductDao productDao) {
+		this.productDao = productDao;
+	}
+
+	/**
+	 * Gets the all products.
+	 *
+	 * @return the all products
+	 */
+	@Override
+	public Iterable<Product> getAllProducts() {
+		return productDao.findAll();
+	}
+
+	/**
+	 * Gets the dashboard products.
+	 *
+	 * @return the all products
+	 */
+	@Override
+	public Iterable<Product> getDashboardProducts() {
+		return productDao.findDashboard();
+	}
+
+	/**
+	 * Gets the product by id.
+	 *
+	 * @param id
+	 *            the id
+	 * @return the product by id
+	 */
+	@Override
+	public Product getProductById(int id) {
+		return productDao.findOne(id);
+	}
+
+	/**
+	 * Delete product by id.
+	 *
+	 * @param id
+	 *            the id
+	 * @return the status
+	 */
+	@Override
+	public Status deleteProductById(int id) {
+		return productDao.delete(id);
+	}
+
+	/**
+	 * Adds the product.
+	 *
+	 * @param entity
+	 *            the entity
+	 * @return the status
+	 */
+	@Override
+	public Status addProduct(Product entity) {
+		return productDao.add(entity);
+	}
+
+	/**
+	 * Update product.
+	 *
+	 * @param entity
+	 *            the entity
+	 * @param id
+	 *            the id
+	 * @return the status
+	 */
+	@Override
+	public Status updateProduct(Product entity, int id) {
+		return productDao.updateProduct(entity, id);
+	}
+}
